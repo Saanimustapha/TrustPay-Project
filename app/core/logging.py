fb8aed61e91d499e8a887bcb46fce0d1d82b95a9
Saani Mustapha <deishini2003@gmail.com>
2026-07-31 09:32:28 +0000

    setup structured logging

---
+++ app/core/logging.py
@@ -0,0 +1,37 @@
+import logging
+import sys
+
+import structlog
+
+
+def setup_logging(environment: str = "local") -> None:
+    shared_processors = [
+        structlog.contextvars.merge_contextvars,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.add_logger_name,
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+    ]
+
+    if environment == "local":
+        processors = shared_processors + [structlog.dev.ConsoleRenderer()]
+    else:
+        processors = shared_processors + [
+            structlog.processors.dict_tracebacks,
+            structlog.processors.JSONRenderer(),
+        ]
+
+    structlog.configure(
+        processors=processors,
+        wrapper_class=structlog.stdlib.BoundLogger,
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+
+    logging.basicConfig(
+        format="%(message)s",
+        stream=sys.stdout,
+        level=logging.DEBUG if environment == "local" else logging.INFO,
+    )
